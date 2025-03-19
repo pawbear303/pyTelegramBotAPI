7ee943115732a255296edf4147db1e5e22a48968
pawbear303 <pawbear303@gmail.com>
2025-03-19 14:30:15 +0700

    Create echo_bot.py

---
+++ echo_bot.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("7888669237:AAGBCbSBHz2Ya-MNCiMdok4uMqTmnwPj4Nk")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
